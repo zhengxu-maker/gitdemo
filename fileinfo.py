c5f1590359cc0798bef14b956f1a28ce561a7009
zhengxu <392153713@qq.com>
2020-03-24 10:16:54 +0800

    initial update

---
+++ fileinfo.py
@@ -0,0 +1,10 @@
+#Script Name    :fileinfo.py
+#Author         :zhengxu
+#Created        :24th March 2020
+#Last Modified  :
+#Version        :1.0
+#Modifications  :
+
+#Description    :show files information for a given file
+
+
